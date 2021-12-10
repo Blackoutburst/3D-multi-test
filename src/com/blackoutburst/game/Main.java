1fe0cd91434bac3d9aa2d3c2c3a4ec8d5815b0da
Blackoutburst <blackoutburst@hotmail.com>
2021-12-10 16:46:32 +0100

    Updated Bogel version

--- src/com/blackoutburst/game/Main.java
+++ src/com/blackoutburst/game/Main.java
@@ -1,11 +1,8 @@
 package com.blackoutburst.game;
 
-import org.lwjgl.opengl.GL11;
-
 import com.blackoutburst.bogel.core.Display;
 import com.blackoutburst.bogel.core.Display.FullScreenMode;
 import com.blackoutburst.bogel.graphics.Color;
-import com.blackoutburst.bogel.graphics.RenderManager;
 import com.blackoutburst.bogel.graphics.Texture;
 
 public class Main {
@@ -18,17 +15,10 @@ public class Main {
 		Cube.init();
 		
 		texture = new Texture("icon128.png");
-		GL11.glEnable(GL11.GL_DEPTH_TEST);
-		
 		while(display.isOpen()) {
 			display.clear();
-			GL11.glClear(GL11.GL_DEPTH_BUFFER_BIT);
 
-			RenderManager.enableWireFrame();
-			Cube.draw(0, 0, 0, 1);
-			Cube.draw(5, 1, -10, 2);
-			Cube.draw(-5, 1, -10, 2);
-			RenderManager.disableWireFrame();
+			Cube.draw(0, 0, -10, 2);
 			
 			display.update();
 		}
