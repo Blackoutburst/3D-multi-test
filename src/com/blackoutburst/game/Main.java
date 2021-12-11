a594c809909443841f2c343d2c62144599e4d0d7
Blackoutburst <blackoutburst@hotmail.com>
2021-12-11 16:27:01 +0100

    Ability to place and destroy blocks

--- src/com/blackoutburst/game/Main.java
+++ src/com/blackoutburst/game/Main.java
@@ -1,6 +1,7 @@
 package com.blackoutburst.game;
 
 import com.blackoutburst.bogel.core.Display;
+import com.blackoutburst.bogel.core.Mouse;
 import com.blackoutburst.bogel.graphics.Color;
 import com.blackoutburst.bogel.graphics.Shape;
 import com.blackoutburst.bogel.graphics.Texture;
@@ -14,15 +15,24 @@ import java.util.List;
 
 public class Main {
 
+	private enum Face {
+		TOP,
+		BOTTOM,
+		FRONT,
+		LEFT,
+		BACK,
+		RIGHT
+	}
+
 	public static Matrix4f projection;
 	public static double closest = 0;
 
 	public static void main(String[] args) {
-		Display display = new Display().setClearColor(new Color(76.0f / 255.0f, 124.0f / 255.0f, 156.0f / 255.0f)).create();
+		Display display = new Display().setFullscreenMode(Display.FullScreenMode.BORDERLESS).setClearColor(new Color(76.0f / 255.0f, 124.0f / 255.0f, 156.0f / 255.0f)).create();
 
 		projection = new Matrix4f();
 		projection.setIdentity();
-		Matrix4f.projectionMatrix(70, 1000, 0.01f, projection);
+		Matrix4f.projectionMatrix(90, 1000, 0.01f, projection);
 
 		GLFW.glfwSetInputMode(Display.getWindow(), GLFW.GLFW_CURSOR, GLFW.GLFW_CURSOR_DISABLED);
 
@@ -51,6 +61,9 @@ public class Main {
 
 			Cube selected = null;
 			closest = 100;
+			int selectedId = -1;
+			int idx = 0;
+			Face face = null;
 			for (Cube c : cubes) {
 				double distance = Math.sqrt(
 						Math.pow((Camera.position.x - c.position.x), 2) +
@@ -61,14 +74,14 @@ public class Main {
 					color = c.interact();
 				}
 
-				/*
-				if (color.r == 255 && color.g == 0 && color.b == 0) System.out.println("FRONT");
-				if (color.r == 0 && color.g == 255 && color.b == 0) System.out.println("BACK");
-				if (color.r == 0 && color.g == 0 && color.b == 255) System.out.println("LEFT");
-				if (color.r == 255 && color.g == 255 && color.b == 0) System.out.println("BOTTOM");
-				if (color.r == 255 && color.g == 0 && color.b == 255) System.out.println("RIGHT");
-				if (color.r == 0 && color.g == 255 && color.b == 255) System.out.println("TOP");
-				*/
+
+				if (color.r == 255 && color.g == 0 && color.b == 0) face = Face.FRONT;
+				if (color.r == 0 && color.g == 255 && color.b == 0) face = Face.BACK;
+				if (color.r == 0 && color.g == 0 && color.b == 255) face = Face.LEFT;
+				if (color.r == 255 && color.g == 255 && color.b == 0) face = Face.BOTTOM;
+				if (color.r == 255 && color.g == 0 && color.b == 255) face = Face.RIGHT;
+				if (color.r == 0 && color.g == 255 && color.b == 255) face = Face.TOP;
+
 
 				if (color.r != 0 || color.g != 0 || color.b != 0) {
 					double dist = Math.sqrt(
@@ -78,7 +91,35 @@ public class Main {
 					if (dist < closest) {
 						closest = dist;
 						selected = new Cube(null, c.position.copy(), new Vector3f(1.01f), c.rotation.copy(), new Color(1,1,1,0.5f));
+						selectedId = idx;
 					}
+				}
+				idx++;
+			}
+
+			if (selectedId != -1 && Mouse.getLeftButton().isPressed())
+				cubes.remove(selectedId);
+
+			if (face != null && selectedId != -1 && Mouse.getRightButton().isPressed()) {
+				switch (face) {
+					case TOP:
+						cubes.add(new Cube(bricks, new Vector3f(selected.position.x, selected.position.y + 1, selected.position.z), new Vector3f(1), new Vector3f(), Color.WHITE));
+					break;
+					case BOTTOM:
+						cubes.add(new Cube(bricks, new Vector3f(selected.position.x, selected.position.y - 1, selected.position.z), new Vector3f(1), new Vector3f(), Color.WHITE));
+					break;
+					case FRONT:
+						cubes.add(new Cube(bricks, new Vector3f(selected.position.x, selected.position.y, selected.position.z - 1), new Vector3f(1), new Vector3f(), Color.WHITE));
+					break;
+					case LEFT:
+						cubes.add(new Cube(bricks, new Vector3f(selected.position.x - 1, selected.position.y, selected.position.z), new Vector3f(1), new Vector3f(), Color.WHITE));
+					break;
+					case BACK:
+						cubes.add(new Cube(bricks, new Vector3f(selected.position.x, selected.position.y, selected.position.z + 1), new Vector3f(1), new Vector3f(), Color.WHITE));
+					break;
+					case RIGHT:
+						cubes.add(new Cube(bricks, new Vector3f(selected.position.x + 1, selected.position.y, selected.position.z), new Vector3f(1), new Vector3f(), Color.WHITE));
+					break;
 				}
 			}
 
