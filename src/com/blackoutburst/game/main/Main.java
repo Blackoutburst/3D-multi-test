6ef4cd86bc94722a46f4e10a90f2ef627d6f53b1
Florian Chanson <f.chanson@barbablanka.com>
2022-04-26 17:00:33 +0200

    Ability to loada a new map using right click

--- src/com/blackoutburst/game/main/Main.java
+++ src/com/blackoutburst/game/main/Main.java
@@ -2,6 +2,7 @@ package com.blackoutburst.game.main;
 
 import com.blackoutburst.bogel.core.Display;
 import com.blackoutburst.bogel.core.Keyboard;
+import com.blackoutburst.bogel.core.Mouse;
 import com.blackoutburst.bogel.graphics.Color;
 import com.blackoutburst.bogel.graphics.RenderManager;
 import com.blackoutburst.bogel.graphics.Texture;
@@ -21,7 +22,7 @@ public class Main {
 
     public static void main(String[] args) {
         //Create the window
-        Display display = new Display().setFullscreenMode(Display.FullScreenMode.FULL).setTitle("Meinraft").create();
+        Display display = new Display().setFullscreenMode(Display.FullScreenMode.NONE).setTitle("Meinraft").create();
 
         //Init projection matrix
         Matrix.projectionMatrix(90, 1000, 0.1F, projection);
@@ -35,6 +36,7 @@ public class Main {
         Cube.init();
 
         Grass.init();
+        Water.init();
 
         //Disable the mouse cursor / hide it
         GLFW.glfwSetInputMode(Display.getWindow(), GLFW.GLFW_CURSOR, GLFW.GLFW_CURSOR_DISABLED);
@@ -44,45 +46,94 @@ public class Main {
         List<Cube> map = new ArrayList<>();
         List<Grass> grass = new ArrayList<>();
 
-        int A = 256;
+        int A = 350;
         Texture t = new Texture("grass.png");
         Texture t2 = new Texture("tallgrass.png");
         for (int x = -A; x < A; x++) {
             for (int z = -A; z < A; z++) {
                 map.add(new Cube(t, new Vector3f(x, (int) (OpenSimplex2.noise2(8, x / 100f, z / 100f) * 7), z), new Vector3f(1), new Vector3f()));
-                if ((int) (OpenSimplex2.noise2(8, x / 100f, z / 100f) * 7) > -2.0)
+                if ((int) (OpenSimplex2.noise2(8, x / 100f, z / 100f) * 7) > -2.0) {
                     grass.add(new Grass(t2, new Vector3f(x + new Random().nextFloat() / 2.0f, (int) (OpenSimplex2.noise2(8, x / 100f, z / 100f) * 7) + 0.7f, z + new Random().nextFloat() / 2.0f), new Vector3f(1), new Vector3f()));
+                }
             }
         }
-        final List<Cube> tmp = new ArrayList<>(map);
-        final int cubesNumber = tmp.size();
+        List<Cube> tmp = new ArrayList<>(map);
+        int cubesNumber = tmp.size();
 
         Cube.setCubeOffset(cubesNumber, tmp);
 
-        final List<Grass> tmp2 = new ArrayList<>(grass);
-        final int gn = tmp2.size();
+        List<Grass> tmp2 = new ArrayList<>(grass);
+        int gn = tmp2.size();
 
         Grass.setCubeOffset(gn, tmp2);
 
-        Water.init();
 
         List<Water> water = new ArrayList<>();
 
         for (float x = -A; x < A; x++) {
             for (float z = -A; z < A ; z++) {
-                if ((int) (OpenSimplex2.noise2(8, x / 100f, z / 100f) * 7) < 0.5)
+                if ((int) (OpenSimplex2.noise2(8, x / 100f, z / 100f) * 7) < -2.0)
                     water.add(new Water(t, new Vector3f(x, -2.2f, z), new Vector3f(1, 1, 1), new Vector3f()));
             }
         }
+        System.out.println(water.size());
 
-        final List<Water> wat = new ArrayList<>(water);
-        final int watNumber = wat.size();
+        List<Water> wat = new ArrayList<>(water);
+        int watNumber = wat.size();
 
         Water.setCubeOffset(watNumber, wat);
+
 
         while (display.isOpen()) {
             //Clear both depth and color buffer
             display.clear();
+
+            if (Mouse.getRightButton().isPressed()) {
+                map.clear();
+                wat.clear();
+                grass.clear();
+                tmp.clear();
+                tmp2.clear();
+                water.clear();
+
+                final int SEED = new Random().nextInt();
+                map = new ArrayList<>();
+                grass = new ArrayList<>();
+
+                A = 350;
+                for (int x = -A; x < A; x++) {
+                    for (int z = -A; z < A; z++) {
+                        map.add(new Cube(t, new Vector3f(x, (int) (OpenSimplex2.noise2(SEED, x / 100f, z / 100f) * 7), z), new Vector3f(1), new Vector3f()));
+                        if ((int) (OpenSimplex2.noise2(SEED, x / 100f, z / 100f) * 7) > -2.0) {
+                            grass.add(new Grass(t2, new Vector3f(x + new Random().nextFloat() / 2.0f, (int) (OpenSimplex2.noise2(SEED, x / 100f, z / 100f) * 7) + 0.7f, z + new Random().nextFloat() / 2.0f), new Vector3f(1), new Vector3f()));
+                        }
+                    }
+                }
+                tmp = new ArrayList<>(map);
+                cubesNumber = tmp.size();
+
+                Cube.setCubeOffset(cubesNumber, tmp);
+
+                tmp2 = new ArrayList<>(grass);
+                gn = tmp2.size();
+
+                Grass.setCubeOffset(gn, tmp2);
+
+                water = new ArrayList<>();
+
+                for (float x = -A; x < A; x++) {
+                    for (float z = -A; z < A ; z++) {
+                        if ((int) (OpenSimplex2.noise2(SEED, x / 100f, z / 100f) * 7) < -2.0)
+                            water.add(new Water(t, new Vector3f(x, -2.2f, z), new Vector3f(1, 1, 1), new Vector3f()));
+                    }
+                }
+
+                wat = new ArrayList<>(water);
+                watNumber = wat.size();
+
+                Water.setCubeOffset(watNumber, wat);
+
+            }
 
             //Close the window when pressing escape
             if (Keyboard.isKeyDown(Keyboard.ESCAPE)) {
