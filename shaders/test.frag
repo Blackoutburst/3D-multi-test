db105489d4d8433062982270205f75604cfa86de
Florian Chanson <f.chanson@barbablanka.com>
2022-05-02 14:58:40 +0200

    Removed noise clamp

--- shaders/test.frag
+++ shaders/test.frag
@@ -1,5 +1,49 @@
-#version 120
+#version 410
 
-void main() {
-    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+uniform float time;
+uniform vec2 resolution;
+#define PI 3.14159265358979
+
+vec3 color = 0.3*vec3(1.0,0.0,0.3);
+float d2y(float d){return 1./(0.2+d);}
+float radius = 0.3;
+
+out vec4 FragColor;
+
+float fct(vec2 p, float r){
+    float a = 3.*mod(atan(p.y, p.x)+time, 2.*PI);
+
+
+    float scan = 0.*1.;
+    return (d2y(a)+scan)*(1.-step(radius,r));
+}
+
+
+float circle(vec2 p, float r){
+    float d=distance(r, radius);
+    return d2y(10.*d);
+}
+
+float grid(vec2 p, float y){
+    float a = 0.2;
+    float res = 23.141592653589;
+    float e = 0.1;
+    vec2 pi = fract(p*res);
+    pi = step(e, pi);
+    return a * y * pi.x * pi.y;
+}
+
+void main( void ) {
+
+    vec2 position = (( gl_FragCoord.xy )-0.5*resolution)/ resolution.y ;
+    position/=cos(.0*length(position));
+    float y  = 0.0;
+
+    float dc = length(position);
+
+    y+=fct(position, dc);
+    y+=circle(position, dc);
+    y+=grid(position, y);
+    y=pow(y,1.80);
+    FragColor = vec4( sqrt(y)*color,1. );
 }
--- src/com/blackoutburst/game/main/Main.java
+++ src/com/blackoutburst/game/main/Main.java
@@ -1,14 +1,15 @@
 package com.blackoutburst.game.main;
 
-import com.blackoutburst.bogel.core.Display;
-import com.blackoutburst.bogel.core.Keyboard;
-import com.blackoutburst.bogel.core.Mouse;
+import com.blackoutburst.bogel.core.*;
 import com.blackoutburst.bogel.graphics.Color;
 import com.blackoutburst.bogel.graphics.RenderManager;
+import com.blackoutburst.bogel.graphics.Shape;
 import com.blackoutburst.bogel.graphics.Texture;
 import com.blackoutburst.bogel.maths.Matrix;
+import com.blackoutburst.bogel.maths.Vector2f;
 import com.blackoutburst.bogel.maths.Vector3f;
 import com.blackoutburst.game.core.*;
+import com.blackoutburst.game.core.Camera;
 import org.lwjgl.glfw.GLFW;
 
 import java.util.ArrayList;
@@ -41,19 +42,21 @@ public class Main {
         //Disable the mouse cursor / hide it
         GLFW.glfwSetInputMode(Display.getWindow(), GLFW.GLFW_CURSOR, GLFW.GLFW_CURSOR_DISABLED);
 
-        RenderManager.enableDepth();
-
         List<Cube> map = new ArrayList<>();
         List<Grass> grass = new ArrayList<>();
 
-        int A = 350;
+        final int A = 256;
+        final int SEED = new Random().nextInt();
+        final int HEIGHT = 12;
+        final float SMOOTH = 200;
+        System.out.println("Seed: " + SEED);
         Texture t = new Texture("grass.png");
         Texture t2 = new Texture("tallgrass.png");
         for (int x = -A; x < A; x++) {
             for (int z = -A; z < A; z++) {
-                map.add(new Cube(t, new Vector3f(x, (int) (OpenSimplex2.noise2(8, x / 100f, z / 100f) * 7), z), new Vector3f(1), new Vector3f()));
-                if ((int) (OpenSimplex2.noise2(8, x / 100f, z / 100f) * 7) > -2.0) {
-                    grass.add(new Grass(t2, new Vector3f(x + new Random().nextFloat() / 2.0f, (int) (OpenSimplex2.noise2(8, x / 100f, z / 100f) * 7) + 0.7f, z + new Random().nextFloat() / 2.0f), new Vector3f(1), new Vector3f()));
+                map.add(new Cube(t, new Vector3f(x, OpenSimplex2.noise2(SEED, x / SMOOTH, z / SMOOTH) * HEIGHT, z), new Vector3f(1), new Vector3f()));
+                if (OpenSimplex2.noise2(SEED, x / SMOOTH, z / SMOOTH) * HEIGHT > -2.0) {
+                    grass.add(new Grass(t2, new Vector3f(x + (new Random().nextFloat() * 2 - 1) / 2.0f,  OpenSimplex2.noise2(SEED, x / SMOOTH, z / SMOOTH) * HEIGHT + 0.7f, z + (new Random().nextFloat() * 2 - 1) / 2.0f), new Vector3f(1), new Vector3f()));
                 }
             }
         }
@@ -72,84 +75,50 @@ public class Main {
 
         for (float x = -A; x < A; x++) {
             for (float z = -A; z < A ; z++) {
-                if ((int) (OpenSimplex2.noise2(8, x / 100f, z / 100f) * 7) < -2.0)
+                if (OpenSimplex2.noise2(SEED, x / SMOOTH, z / SMOOTH) * HEIGHT < -2.0)
                     water.add(new Water(t, new Vector3f(x, -2.2f, z), new Vector3f(1, 1, 1), new Vector3f()));
             }
         }
-        System.out.println(water.size());
 
         List<Water> wat = new ArrayList<>(water);
         int watNumber = wat.size();
 
         Water.setCubeOffset(watNumber, wat);
 
+        Shape skybox = new Shape(Shape.ShapeType.QUAD, new Vector2f(Display.getFramebufferSize().x / 2.0f,Display.getFramebufferSize().y / 3.0f), new Vector2f(1407 * 2.1f, 2000 * 2.1f), 0, false);
+        skybox.setColor(new Color(1,1,1, 0.5f));
+        Shader v = Shader.loadShader(Shader.VERTEX, "quad.vert");
+        Shader f = Shader.loadShader(Shader.FRAGMENT, "test2.frag");
+        ShaderProgram skyboxProgram = new ShaderProgram(v, f);
+        skybox.setShaderProgram(skyboxProgram);
+        skybox.getShaderProgram().setUniform2f("resolution", new Vector2f(Display.getFramebufferSize().x,Display.getFramebufferSize().y));
 
+        System.gc();
         while (display.isOpen()) {
             //Clear both depth and color buffer
             display.clear();
-
-            if (Mouse.getRightButton().isPressed()) {
-                map.clear();
-                wat.clear();
-                grass.clear();
-                tmp.clear();
-                tmp2.clear();
-                water.clear();
-
-                final int SEED = new Random().nextInt();
-                map = new ArrayList<>();
-                grass = new ArrayList<>();
-
-                A = 350;
-                for (int x = -A; x < A; x++) {
-                    for (int z = -A; z < A; z++) {
-                        map.add(new Cube(t, new Vector3f(x, (int) (OpenSimplex2.noise2(SEED, x / 100f, z / 100f) * 7), z), new Vector3f(1), new Vector3f()));
-                        if ((int) (OpenSimplex2.noise2(SEED, x / 100f, z / 100f) * 7) > -2.0) {
-                            grass.add(new Grass(t2, new Vector3f(x + new Random().nextFloat() / 2.0f, (int) (OpenSimplex2.noise2(SEED, x / 100f, z / 100f) * 7) + 0.7f, z + new Random().nextFloat() / 2.0f), new Vector3f(1), new Vector3f()));
-                        }
-                    }
-                }
-                tmp = new ArrayList<>(map);
-                cubesNumber = tmp.size();
-
-                Cube.setCubeOffset(cubesNumber, tmp);
-
-                tmp2 = new ArrayList<>(grass);
-                gn = tmp2.size();
-
-                Grass.setCubeOffset(gn, tmp2);
-
-                water = new ArrayList<>();
-
-                for (float x = -A; x < A; x++) {
-                    for (float z = -A; z < A ; z++) {
-                        if ((int) (OpenSimplex2.noise2(SEED, x / 100f, z / 100f) * 7) < -2.0)
-                            water.add(new Water(t, new Vector3f(x, -2.2f, z), new Vector3f(1, 1, 1), new Vector3f()));
-                    }
-                }
-
-                wat = new ArrayList<>(water);
-                watNumber = wat.size();
-
-                Water.setCubeOffset(watNumber, wat);
-
-            }
 
             //Close the window when pressing escape
             if (Keyboard.isKeyDown(Keyboard.ESCAPE)) {
                 display.close();
             }
+            skybox.getShaderProgram().setUniform1f("time", (float) Time.getRuntime());
+            skybox.getShaderProgram().setUniform3f("cameraPosition", Camera.position);
 
             //Update the camera
             Camera.update();
 
             Cube.draw(cubesNumber);
 
+            RenderManager.disableCulling();
             Water.draw(watNumber);
 
-            RenderManager.disableCulling();
             Grass.draw(gn);
             RenderManager.enableCulling();
+
+            RenderManager.disableDepth();
+            skybox.draw();
+            RenderManager.enableDepth();
 
             //Swap buffer
             display.update();
