ff96b05a521ade32cce833d8138905556ba42dd8
Blackoutburst <blackoutburst@hotmail.com>
2024-04-21 12:13:22 +0200

    Working kotlin

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,83 @@
+import org.lwjgl.Version
+import org.lwjgl.glfw.Callbacks
+import org.lwjgl.glfw.GLFW
+import org.lwjgl.glfw.GLFWErrorCallback
+import org.lwjgl.opengl.GL
+import org.lwjgl.opengl.GL11
+import org.lwjgl.system.MemoryStack
+import org.lwjgl.system.MemoryUtil
+
+
+fun main() {
+    Main().run()
+}
+
+class Main {
+    private var window: Long = 0
+
+    fun run() {
+        println("Hello LWJGL " + Version.getVersion() + "!")
+
+        init()
+        loop()
+
+        Callbacks.glfwFreeCallbacks(window)
+        GLFW.glfwDestroyWindow(window)
+
+        GLFW.glfwTerminate()
+        GLFW.glfwSetErrorCallback(null)!!.free()
+    }
+
+    private fun init() {
+        GLFWErrorCallback.createPrint(System.err).set()
+
+        check(GLFW.glfwInit()) { "Unable to initialize GLFW" }
+
+        GLFW.glfwDefaultWindowHints()
+        GLFW.glfwWindowHint(GLFW.GLFW_VISIBLE, GLFW.GLFW_FALSE)
+        GLFW.glfwWindowHint(GLFW.GLFW_RESIZABLE, GLFW.GLFW_TRUE)
+
+        window = GLFW.glfwCreateWindow(300, 300, "Hello World!", MemoryUtil.NULL, MemoryUtil.NULL)
+        if (window == MemoryUtil.NULL) throw RuntimeException("Failed to create the GLFW window")
+
+        GLFW.glfwSetKeyCallback(window) { window: Long, key: Int, scancode: Int, action: Int, mods: Int ->
+            if (key == GLFW.GLFW_KEY_ESCAPE && action == GLFW.GLFW_RELEASE) GLFW.glfwSetWindowShouldClose(
+                window,
+                true
+            )
+        }
+
+        MemoryStack.stackPush().use { stack ->
+            val pWidth = stack.mallocInt(1)
+            val pHeight = stack.mallocInt(1)
+
+
+            GLFW.glfwGetWindowSize(window, pWidth, pHeight)
+
+            val vidmode = GLFW.glfwGetVideoMode(GLFW.glfwGetPrimaryMonitor())
+
+            GLFW.glfwSetWindowPos(
+                window,
+                (vidmode!!.width() - pWidth[0]) / 2,
+                (vidmode.height() - pHeight[0]) / 2
+            )
+        }
+        GLFW.glfwMakeContextCurrent(window)
+        GLFW.glfwSwapInterval(1)
+        GLFW.glfwShowWindow(window)
+    }
+
+    private fun loop() {
+        GL.createCapabilities()
+
+        GL11.glClearColor(1.0f, 0.0f, 0.0f, 0.0f)
+
+        while (!GLFW.glfwWindowShouldClose(window)) {
+            GL11.glClear(GL11.GL_COLOR_BUFFER_BIT or GL11.GL_DEPTH_BUFFER_BIT)
+
+            GLFW.glfwSwapBuffers(window)
+
+            GLFW.glfwPollEvents()
+        }
+    }
+}
