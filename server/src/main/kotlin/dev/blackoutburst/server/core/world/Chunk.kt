e6017cd2af9127c8d6f623de9eaa51aa6a69477a
Rafale25 <rafale25.dev@gmail.com>
2024-07-05 14:47:35 +0200

    improved generation wip

--- server/src/main/kotlin/dev/blackoutburst/server/core/world/Chunk.kt
+++ server/src/main/kotlin/dev/blackoutburst/server/core/world/Chunk.kt
@@ -2,6 +2,7 @@ package dev.blackoutburst.server.core.world
 
 import dev.blackoutburst.server.maths.Vector3i
 import dev.blackoutburst.server.utils.OpenSimplex2
+import kotlin.math.pow
 
 class Chunk {
     var position: Vector3i
@@ -99,13 +100,70 @@ class Chunk {
     }
 
 
-    private fun getType(x:Int, y: Int, z: Int): BlockType {
-        val height = (OpenSimplex2.noise2(World.seed, x / 200.0, z / 200.0) * 20).toInt() + 10
+    private fun octaveNoise2D(x: Float, z: Float, octaves: Int): Float {
+        var h: Float = 0.0f
 
-        return if (y == height) BlockType.GRASS
-        else if (y < height - 3) BlockType.STONE
-        else if (y > height) BlockType.AIR
-        else BlockType.DIRT
+        for (i in 0..<octaves) {
+            val v = OpenSimplex2.noise2(World.seed, // [-1, 1]
+                        x.toDouble() * (i+1f) * 4f,
+                        z.toDouble() * (i+1f) * 4f
+                    )
+            h += v / 2f.pow(i)
+        }
+
+        return h
     }
+
+    private fun octaveNoise3D(x: Float, y: Float, z: Float, octaves: Int, fallOff: Float = 2f): Float {
+        var h: Float = 0.0f
+
+        for (i in 0..octaves) {
+            val v = OpenSimplex2.noise3_ImproveXZ(World.seed,
+                x.toDouble() * (i+1f) * 4f,
+                y.toDouble() * (i+1f) * 4f,
+                z.toDouble() * (i+1f) * 4f)
+            h += v / fallOff.pow(i)
+        }
+
+        return h
+    }
+
+    private fun continentalnessCurve(x: Float): Float {
+//        if (x > 1f) return 0.7646f
+//        if (x < -1f) return -0.7646f
+        return kotlin.math.sin(x * x * x * 0.6f) + 0.2f
+    }
+
+    private fun lerp(a: Float, b: Float, t: Float): Float {
+        return a * (1f - t) + (b * t)
+    }
+
+    private fun mapRange(value: Float, min1: Float, max1: Float, min2: Float, max2: Float): Float {
+        return min2 + (value - min1) * (max2 - min2) / (max1 - min1)
+    }
+
+    private fun getType(x: Int, y: Int, z: Int): BlockType {
+        val continentalnessNoiseScaleFactor = 1f / 900f
+        val continentalness = continentalnessCurve(octaveNoise2D(x * continentalnessNoiseScaleFactor, z * continentalnessNoiseScaleFactor, 3)) // 2D [-1, 1]
+        val erosionNoiseScaleFactor = 1f / 1800f
+        val erosion = octaveNoise2D(x * erosionNoiseScaleFactor, z * erosionNoiseScaleFactor, 2) // 2D [-1, 1]
+//        val peaksAndValleys
+
+        val densityNoiseScaleFactor = 1f / 600f
+        val density = octaveNoise3D(x * densityNoiseScaleFactor, y * densityNoiseScaleFactor, z * densityNoiseScaleFactor, 5, 1.3f) // 3D [-1, 1]
+
+
+        val normalizedHeight = y / 128f
+        val squashing = 0.21f * (1f - mapRange(erosion, -1f, 1f, 0f, 1f)) // using erosion to affect squashing
+        val fill = density * squashing > normalizedHeight - continentalness * 0.4f
+
+        if (fill) {
+            if (y > 0f) return BlockType.GRASS
+            if (y == 0) return BlockType.OAK_LOG
+            return BlockType.STONE
+        }
+        return BlockType.AIR
+    }
+
 
 }
